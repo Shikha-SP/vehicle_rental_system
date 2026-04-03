1f2159b7d9326235e84aa2708ca95a2910b25814
Shikha-SP <pandeyshikha567@gmail.com>
2026-04-03 13:54:50 +0545

    Add vehicle management and renter module

---
+++ public/renter/list_Car.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+// Ensure the user is logged in and is NOT admin
+if (!isset($_SESSION['user_id']) || (!empty($_SESSION['is_admin']))) {
+    header("Location: ../landing_page.php");
+    exit;
+}
+
+$username = $_SESSION['username'] ?? 'User';
+?>
+
+<?php require_once '../../includes/header.php'; ?>
+<main>
+
+<ul>
+    <li><a href="../vehicle/add.php">List Your Car</a></li>
+</ul>
+</main>
+<?php require_once '../../includes/footer.php'; ?>
---
+++ public/vehicle/vehicles.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// Ensure the user is logged in and is NOT admin
+if (!isset($_SESSION['user_id']) || (!empty($_SESSION['is_admin']))) {
+    header("Location: ../landing_page.php");
+    exit;
+}
+
+$username = $_SESSION['username'] ?? 'User';
+?>
+
+<?php require_once '../../includes/header.php'; ?>
+<main>
+
+<ul>
+    <li><a href="../vehicle/add.php">View Vehicles</a></li>
+    
+</ul>
+</main>
+<?php require_once '../../includes/footer.php'; ?>
