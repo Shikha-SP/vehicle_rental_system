7bd415a5e72a0065dc5e6f48e5954582907857d5
mendo9988 <np03cs4a240305@heraldcollege.edu.np>
2026-04-06 08:48:46 +0545

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<nav class="dashboard-nav">
+    <div class="admin-title">
+        <h3>Admin Console</h3>
+        <h3 id="fleet">FLEET MANAGEMENT</h3>
+    </div>
+    <ul class="nav-list">
+        <li class="nav-item"><i class="fa-solid fa-qrcode"></i>Dashboard</li>
+        <li class="nav-item"><i class="fa-regular fa-calendar"></i>Reservations</li>
+        <li class="nav-item"><i class="fa-solid fa-car"></i>Fleet</li>
+        <li class="nav-item"><i class="fa-solid fa-chart-column"></i>Analytics</li>
+        <li class="nav-item"><i class="fa-solid fa-gear"></i>Settings</li>
+    </ul>
+    <button class="add-vehicle-btn">+ Add Vehicle</button>
+</nav>
---
+++ public/admin/reservations.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/vehicle_rental_system/assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+     <div class="operations-control">
+         <nav class="dashboard-nav">
+                 <ul class="nav-list">
+                     <li class="nav-item">DASHBOARD</li>
+                     <li class="nav-item">RESERVATIONS</li>
+                     <li class="nav-item">VEHICLES</li>
+                     <li class="nav-item">CUSTOMERS</li>
+                     <li class="nav-item">ANALYTICS</li>
+                     <li class="nav-item">SETTINGS</li>
+                 </ul>
+                 <button class="add-vehicle-btn">+ ADD VEHICLE</button>
+        </nav>
+
+        <!-- Top Header -->
+        <header class="status-header">
+            <h1 class="status-title">RESERVATIONS</h1>
+            <div class="totals">
+                <p>42 Total Vehicles</p>
+                <p>12 In Maintenance</p>
+                <p>30 Active</p>
+            </div>
+            
+        </header>
+
+        <div>
+
+        </div>
+     </div>
+</body>
+</html>
