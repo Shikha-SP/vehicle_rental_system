39ffd95ba3897a30216163b0d9da9369401e51fd
mendo9988 <np03cs4a240305@heraldcollege.edu.np>
2026-04-11 16:16:31 +0545

    new commit

--- public/user/index.php
+++ public/user/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /**
  * public/user/index.php  —  TD Rentals homepage (featured car + booking widget)
  */
